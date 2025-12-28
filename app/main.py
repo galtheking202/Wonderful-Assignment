8ad72d1eea13c8647ed587e774852b807a88863d
galtheking202 <47147613+galtheking202@users.noreply.github.com>
2025-12-28 15:46:48 +0200

    removed the base models

--- app/main.py
+++ app/main.py
@@ -1,39 +1,24 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import StreamingResponse
-from pydantic import BaseModel
 from agent_utils.agent import MedicineAssistantAgent
+from logger import LOG_BUFFER
 from pathlib import Path
 import uvicorn
-from logger import LOG_BUFFER
-from pymongo import MongoClient
+
 app = FastAPI()
 BASE_DIR = Path(__file__).resolve().parent
 
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
-# Request model
-class AgentRequest(BaseModel):
-    """Request model for agent invocation."""
-    prompt: str
-
-
-# Response model
-class AgentResponse(BaseModel):
-    """Response model for agent invocation."""
-    response: str
-    
-
 @app.get("/")
-async def home(request: Request):
+async def home(request: str):
     """Serve the main HTML interface."""
     return templates.TemplateResponse("index.html", {"request": request})
 
-
-
 @app.post("/agent")
-async def invoke_agent(request: AgentRequest):
-    if not request.prompt.strip():
+async def invoke_agent(request: str):
+    if not request.strip():
         raise HTTPException(status_code=400, detail="Prompt cannot be empty")
 
     agent = MedicineAssistantAgent()
@@ -55,9 +40,5 @@ def get_logs():
     return {
         "logs": "\n".join(LOG_BUFFER)
     }
-
-mongo_client = MongoClient("mongodb://root:example@mongo:27017")
-db = mongo_client["mydb"]
-print(list(db["medicens_stock"].find()))
 
 uvicorn.run(app, host="0.0.0.0", port=8888)
